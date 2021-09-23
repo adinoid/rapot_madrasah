2f1968f745cfcd3811bb2ce23d6a3e3bd92bbec8
adinoid <ali.fahruddin.046@gmail.com>
2021-09-23 14:39:50 +0700

    first init

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+$host = "localhost";
+$databaseuser = "root";
+$databasename = "mi02mazro_rapot";
+$dbpassword = "";
+define("DB_SERVER",$host);
+define("DB_USERNAME",$databaseuser);
+define("DB_PASSWORD",$dbpassword);
+define("DB_DATABASE",$databasename);
+?>
